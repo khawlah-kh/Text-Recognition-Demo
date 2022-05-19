1f9a24224462377209f1e6a5a67dd708a308c1f2
khawlah-kh <44741544+khawlah-kh@users.noreply.github.com>
2022-05-19 12:03:17 +0300

    Initial Commit

---
+++ Shared/Text_Recognition_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Text_Recognition_DemoApp.swift
+//  Shared
+//
+//  Created by khawlah khalid on 19/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct Text_Recognition_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
