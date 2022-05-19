ba820df0b5f2d92ec2625f0f81611e8a2ad65aaf
khawlah-kh <44741544+khawlah-kh@users.noreply.github.com>
2022-05-19 14:28:39 +0300

    It is working !

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,11 +6,59 @@
 //
 
 import SwiftUI
-
+import Vision
 struct ContentView: View {
+    @State var recognisedText = ""
+    let uiImage = UIImage(named: "Image2")
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Text("Text Recognition Example")
+                .padding()
+            Image(uiImage: uiImage!)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 300, height: 300)
+            
+            Text(recognisedText)
+                .font(.title2)
+                .padding()
+            
+        }.onAppear {
+            recognizeText (image: uiImage)
+        }
+       
+    }
+    
+    
+    private func recognizeText(image : UIImage?){
+       
+        guard let cgImage = image?.cgImage else{
+           
+            return}
+      //  to recognise a text :
+        //Handler
+        let handler = VNImageRequestHandler(cgImage: cgImage, options: [:])
+       
+        //Request
+        let request = VNRecognizeTextRequest { request, error in
+            guard let observations = request.results as? [VNRecognizedTextObservation],error == nil else {
+                return}
+            let text = observations.compactMap { $0.topCandidates(1).first?.string
+            }.joined(separator: " ")
+            DispatchQueue.main.async {
+                self.recognisedText = text
+
+            }
+           
+        }
+        //Process Request
+        do{
+            try handler.perform([request])
+        }
+        catch{
+            print(error)
+        }
+        
     }
 }
 
